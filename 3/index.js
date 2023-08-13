827772a32d61f9e644eabc4d3c983183dbcf147e
vahidseyyedi <s.vahidseyyedi.159@gmail.com>
2023-08-13 15:30:39 +0330

    change

--- 3/index.js
+++ 3/index.js
@@ -3,11 +3,38 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+let pro = 1;
 
-function receive
+function receive() {
+    return new Promise((resolve) => {
+        rl.question("please enter number: \n", (answer) => {
+            resolve(answer);
+        });
+    });
+}
 
+async function verify() {
+    const result = await receive();
+    if (result < 1) {
+        console.log("no");
+        await verify();
+    }
+    else {
+        console.log(calc(result));
+        rl.close();
+    }
+}
 
+function calc(number) {
+    if (number == 0) {
+        return pro;
+    } else {
+        pro = pro * number;
+        return calc(number - 1);
+    }
+}
 
+verify();
 
 // rl.question("please enter number: \n" , (num)=>{
 // if(num <=0 )
