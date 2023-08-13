f3758be68ac92d0eb9ee9effe2fd463296e32f96
vahidseyyedi <s.vahidseyyedi.159@gmail.com>
2023-08-13 11:46:54 +0330

    writing as a recursive function

--- 1/index.js
+++ 1/index.js
@@ -1,4 +1,5 @@
 const { log } = require('console');
+const { resolve } = require('path');
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
@@ -32,51 +33,99 @@ class team {
     }
 }
 let t;
-rl.question("please enter team name : \n", (teamName) => {
-    rl.question("please enter coach name : \n", (coachTeam) => {
-        t = new team(teamName, coachTeam);
-        console.log("save \n");
-        rl.question("please enter number player: \n", (number) => {
-            add(number);
+
+function receiveTeamName() {
+    return new Promise((resolve, reject) => {
+        rl.question("please enter team name: \n", (teamName) => {
+            resolve(teamName);
         });
     });
-});
+}
 
-function add(num) {
-    if (num == 0) {
+function receiveCoachName() {
+    return new Promise((resolve, reject) => {
+        rl.question("please enter coach name: \n", (coachName) => {
+            resolve(coachName);
+        });
+    });
+}
+
+function receiveNumberPlayer() {
+    return new Promise((resolve, reject) => {
+        rl.question("please enter number of players: \n", (number) => {
+            resolve(number);
+        });
+    });
+}
+
+async function addTeam() {
+    const teamName = await receiveTeamName();
+    const coachName = await receiveCoachName();
+    const numberPlayers = await receiveNumberPlayer();
+    t = new team(teamName, coachName);
+    console.log("save");
+    addPlayerTeam(numberPlayers);
+}
+
+function receiveNamePlayer() {
+    return new Promise((resolve) => {
+        rl.question("Enter player name:", (name) => {
+            resolve(name);
+        });
+    });
+}
+
+function receiveAgePlayer() {
+    return new Promise((resolve) => {
+        rl.question("Enter player age:", (age) => {
+            resolve(age);
+        });
+    });
+}
+
+function receivePositionPlayer() {
+    return new Promise((resolve) => {
+        rl.question("Enter player position:", (position) => {
+            resolve(position);
+        });
+    });
+}
+
+async function addPlayerTeam(number) {
+    if (number == 0) {
         console.log("compleate");
         remove();
     } else {
-        rl.question("Enter player name: ", (namePlayer) => {
-            rl.question("Enter age: ", (agePlayer) => {
-                rl.question("Enter position: ", (positionPlayer) => {
-                    const player1 = new player(namePlayer, agePlayer, positionPlayer);
-                    t.addPlayer(player1);
-                    console.log(player1);
-                    console.log("----------------------");
-                    add(num - 1);
-                });
-            });
-        });
+        const name = await receiveNamePlayer();
+        const age = await receiveAgePlayer();
+        const position = await receivePositionPlayer();
+        const player1 = new player(name, age, position);
+        t.addPlayer(player1);
+        console.log(player1);
+        console.log("----------------------");
+        addPlayerTeam(number - 1);
     }
 }
 
+function receiveNameRemove() {
+    return new Promise((resolve) => {
+        rl.question("please enter name for remove :", (name) => {
+            t.removePlayer(name);
+            resolve(name);
+        });
+    });
+}
+
 function remove() {
-    rl.question("Do you want to remove the player?(y/n)", (answer) => {
-        if (answer == "n" || answer == "N" || answer == "y" || answer == "Y") {
-            if (answer == "n" || answer == "N") {
-                console.log("ok");
-            }
-            else {
-                rl.question("please enter name :", (rName) => {
-                    t.removePlayer(rName);
-                    console.log(`remove ${rName} compleate`);
-                    t.selectall();
-                    remove();
-                });
-            }
+    rl.question("Do you want to remove the player? (y/n)", async (answer) => {
+        if (answer == "y") {
+            console.log(await receiveNameRemove());
+            t.selectall();
+            rl.close();
         } else {
-            console.log("no");
+            console.log("ok");
+            rl.close();
         }
     });
 }
+addTeam();
