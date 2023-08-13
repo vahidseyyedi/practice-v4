5afff2e054ca4e8d24fb8988b8b1064cc1628a1f
vahidseyyedi <s.vahidseyyedi.159@gmail.com>
2023-08-13 16:52:54 +0330

    add folder

--- 11/index.js
+++ 11/index.js
@@ -1,26 +1,31 @@
+const { rejects } = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-let divisonH;
-let leftOverH;
-let min;
-let leftOverM;
-let leftOverS;
-let re;
-rl.question("please enter time : \n", (time) => {
-    if (time < 0 || time > 359999) {
-        console.log("no");
-    } else {
-        divisonH = Math.floor(time / 3600);
-        leftOverH = time % 3600;
-        min = Math.floor(leftOverH / 60);
-        leftOverM = leftOverH % 60;
-        leftOverS = Math.floor(leftOverM % 60);
-        check(divisonH, min, leftOverS);
-    }
-});
+
+function receive(){
+    return new Promise((resolve , rejects) =>{
+        rl.question("please enter time : \n" , (time) =>{
+            if(time < 0 || time > 359999 ){
+                rejects();
+            }else{
+                resolve(time);
+            }
+        });
+    });
+}
+
+async function calc(){
+    const time = await receive()
+    const hour = Math.floor(time / 3600);
+    const leftOverH = time % 3600;
+    const min = Math.floor(leftOverH / 60);
+    const leftOverM = leftOverH % 60;
+    const sec = Math.floor(leftOverM % 60);
+    check(hour , min , sec);
+}
 
 function check(hour, min, sec) {
     if (hour < 10) {
@@ -32,7 +37,9 @@ function check(hour, min, sec) {
     if (sec < 10) {
         sec = "0" + sec;
     }
-    re = `${hour} : ${min} : ${sec}`
+    const re = `${hour} : ${min} : ${sec}`
     console.log(re);
     rl.close();
 }
+
+calc();
