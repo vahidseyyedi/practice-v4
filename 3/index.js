7b983f96acc1abeb2d78839944693445b2e387fd
vahidseyyedi <s.vahidseyyedi.159@gmail.com>
2023-08-16 11:12:05 +0330

    update v4

--- 3/index.js
+++ 3/index.js
@@ -6,22 +6,34 @@ const rl = readline.createInterface({
 let pro = 1;
 
 function receive() {
-    return new Promise((resolve , reject) => {
+    return new Promise((resolve, reject) => {
         rl.question("please enter number: \n", (answer) => {
-            if(an)
+            if (answer == "") {
+                reject("null");
+            } else {
+                if (!Object.is(NaN, parseInt(answer))) {
+                    resolve(parseInt(answer));
+                } else {
+                    reject("no string");
+                }
+            }
+            rl.close();
         });
     });
 }
 
 async function verify() {
-    const result = await receive();
-    if (result < 1) {
-        console.log("no");
-        await verify();
-    }
-    else {
-        console.log(calc(result));
-        rl.close();
+    try {
+        const result = await receive();
+        if (result < 1) {
+            console.log("no");
+            await verify();
+        }
+        else {
+            console.log(calc(result));
+        }
+    } catch (err) {
+        console.log("error: " + err);
     }
 }
 
